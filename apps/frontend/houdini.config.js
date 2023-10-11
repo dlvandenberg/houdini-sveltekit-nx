0e926789e8c66e6ca9c5e5a1f6ed7664e307aa99
Dennis van den Berg <dennis.vandenberg@nedap.com>
2023-10-11 10:15:32 +0200

    Houdini setup (not working)

---
+++ apps/frontend/houdini.config.js
@@ -0,0 +1,11 @@
+/** @type { import('houdini').ConfigFile } */
+const config = {
+	watchSchema: {
+		url: 'https://houdinigraphql.com/graphql'
+	},
+	plugins: {
+		'houdini-svelte': {}
+	}
+};
+
+export default config;
